7095ee1a7cf4fff01038bbc2e9048cba40959ce3
Gelnior <gelnior@free.fr>
2018-03-22 22:57:13 +0100

    Improve root password test results
    Following discussion of #1 issue:
    
    * In case it fails, it checks if no SSH is configured. In that case it's considered as a voluntary action and a warning message is emitted.
    * If both are configured, it's considered as an error

--- pschecker/checks/root_password.py
+++ pschecker/checks/root_password.py
@@ -1,23 +1,31 @@
-import subprocess
+import os
+
+from pschecker.helpers import (
+    get_first_line_from_command,
+    get_lines_from_command
+)
 
 name = "Root must not use password"
 description = "Your root user should not be able to log in with a password."
 
-def run_check(config):
-    result = subprocess.Popen(
-        'passwd -S',
-        shell=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT
-    )
-    first_line = str(result.stdout.readlines()[0])
 
+def run_check(config):
+    first_line = get_first_line_from_command("passwd -S")
     if "NP" in first_line:
         return {"status": "SUCCESS"}
     else:
-        return {
-            "status": "FAILURE",
-            "message": "Your root user should not be able to log in with "
-                       "password, only SSH login should be allowed."
-        }
-
+        lines = get_lines_from_command("cat /root/.ssh/authorized_keys")
+        if os.path.exists("/root/.ssh/authorizeld_keys") and len(lines) > 0:
+            return {
+                "status": "FAILURE",
+                "message": "Your root user should not be able to log in with "
+                           "password, only SSH login should be allowed."
+            }
+        else:
+            return {
+                "status": "WARNING",
+                "message": "Your root user can connect only with root password."
+                           " That's fine but you should consider having a "
+                           "strong password and think about allowing SSH "
+                           "access only."
+            }
