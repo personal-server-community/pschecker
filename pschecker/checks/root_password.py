ab61d7dc841dbcb5219118d0bb8e584c79da1052
Gelnior <gelnior@free.fr>
2018-04-29 23:33:25 +0200

    Simplify root password check

--- pschecker/checks/root_password.py
+++ pschecker/checks/root_password.py
@@ -10,22 +10,14 @@ description = "Your root user should not be able to log in with a password."
 
 
 def run_check(config):
-    first_line = get_first_line_from_command("passwd -S")
-    if "NP" in first_line:
+    first_line = get_first_line_from_command(
+        "cat /etc/ssh/sshd_config | grep PermitRootLogin"
+    )
+    if "without-password" in first_line:
         return {"status": "SUCCESS"}
     else:
-        lines = get_lines_from_command("cat /root/.ssh/authorized_keys")
-        if os.path.exists("/root/.ssh/authorized_keys") and len(lines) > 0:
-            return {
-                "status": "FAILURE",
-                "message": "Your root user should not be able to log in with "
-                           "password, only SSH login should be allowed."
-            }
-        else:
-            return {
-                "status": "WARNING",
-                "message": "Your root user can connect only with root password."
-                           " That's fine but you should consider having a "
-                           "strong password and think about allowing SSH "
-                           "access only."
+        return {
+            "status": "FAILURE",
+            "message": "Your root user should not be able to log in with "
+                       "password, only SSH login should be allowed."
             }
