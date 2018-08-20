09599e8aead907cc704e5b86fbca80c0a1891fc9
Gelnior <gelnior@free.fr>
2018-08-21 00:58:04 +0200

    Improve result display

--- pschecker/cli.py
+++ pschecker/cli.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+import re
 import click
 
+from terminaltables import SingleTable
 from pschecker import checks
 
 
@@ -46,30 +48,43 @@ def display_introduction(config):
     print("- Distribution: %s" % config["distribution"])
     print("- Domain names: %s" % ", ".join(config["domains"]))
     print("")
-    print("Your personal server diagnostic is:")
-    print("")
+    print("Processing tests:")
 
 
 def run_diagnostic(config):
+    table_rows = [["Name", "Result", "Infos"]]
     for check_runner in checks.check_runners:
         result = check_runner.run_check(config)
         if result["status"] != "EMPTY":
-            display_check_result(
+            print(check_runner.name + ": done")
+            table_rows.append(build_check_result_row(
                 check_runner.name,
                 result
-            )
-            print("----")
+            ))
+
+    table = SingleTable(
+        table_rows,
+        title="Your personal server diagnostic"
+    )
+
+    print("")
+    print(table.table)
 
 
-def display_check_result(name, result):
+def build_check_result_row(name, result):
+    message = re.sub(
+        "(.{32})",
+        "\\1\n",
+        result.get("message", ""),
+        0,
+        re.DOTALL
+    )  # Make sure that message is not too wide for display
     if result["status"] == "SUCCESS":
-        print("%s: %s" % (name, GREEN + "OK" + ENDC))
+        return [name, GREEN + "OK" + ENDC, ""]
     elif result["status"] == "WARNING":
-        print("%s: %s" % (name, ORANGE + "WARN" + ENDC))
-        print("* %s" % result["message"])
+        return [name, ORANGE + "WARN" + ENDC, message]
     else:
-        print("%s: %s" % (name, RED + BOLD + "KO" + ENDC))
-        print("* %s" % result["message"])
+        return [name, RED + BOLD + "KO" + ENDC, message]
 
 
 if __name__ == '__main__':
